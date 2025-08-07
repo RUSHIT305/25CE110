af6c8c98f081a004e0129857bce7f975b26bc8e1
RUSHIT SHAH <134259276+RUSHIT305@users.noreply.github.com>
2025-08-07 16:16:17 +0530

    Add files via upload

---
+++ PRACTICAL 9 ALL FILES/practical9.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+void main()
+{
+    float discount;
+    int finalamount;
+    int  totalamount;
+    int amount;
+
+    printf(" enter your amount ");
+    scanf("%d",&totalamount);
+
+    if(amount<=1000)
+    {
+
+     printf("total amount = &d\n", totalamount);
+        printf("final amount =%d\n",finalamount);
+        printf("discount amount : %.2f\n", discount );
+    }
+    else if (totalamount>1000 && totalamount<5000)
+
+    {
+        discount=0.01;
+        finalamount=totalamount-discount;
+         printf("total amount = &d\n", totalamount);
+        printf("final amount =%d\n",finalamount);
+        printf("discount amount =%f\n", discount );
+    }
+
+    else
+        discount=0.05;
+    finalamount=totalamount-discount;
+     printf("total amount = &d\n", totalamount);
+        printf("final amount =%d\n",finalamount);
+        printf("discount amount =%d\n", discount );
+}
