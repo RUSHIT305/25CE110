fb835412e4f2fed6382cded1f535561c86b428ef
RUSHIT SHAH <134259276+RUSHIT305@users.noreply.github.com>
2025-08-19 10:52:37 +0530

    Add files via upload

---
+++ practical 16/prac16.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+    int matchsticks,user,com;
+    int tm=21;
+     while (1){
+    printf("pick no. of matchsticks");
+    scanf("%d",&matchsticks);
+    com=5-matchsticks;
+    printf("computer choice = %d",com);
+
+     }
+
+
+}
