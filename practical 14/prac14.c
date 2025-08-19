cc2eba312001220489dda2d9c90b07a85ab5e17a
RUSHIT SHAH <134259276+RUSHIT305@users.noreply.github.com>
2025-08-19 12:36:09 +0530

    Add files via upload

---
+++ practical 14/prac14.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int amt=5000;
+	int withdrawal_amt , choice ;
+
+	do
+	{
+		printf("\nTotal balance = %d\n1-Withdraw\n0-Exit\nEnter choice:",amt);
+		scanf("%d",&choice);
+
+		switch(choice)
+		{
+			case 1:
+				printf("Enter the withdrawl amount: ");
+				scanf("%d",&withdrawal_amt);
+
+				if(amt<withdrawal_amt)
+				{
+					printf("Insufficient Balance in Account!\n");
+				}
+				else if(withdrawal_amt<0)
+				{
+					printf("Negative Transaction!\nTry Again.\n");
+				}
+				else if(amt-withdrawal_amt<1000)
+				{
+					printf("Cannot proceed!\nThere should remain Rs1000 as balance.\n");
+				}
+				else
+				{
+					printf("Tansaction Completed.\n");
+					amt-=withdrawal_amt;
+				}
+				break;
+
+			case 0:
+				printf("Exiting....\nPress any key to exit.");
+				break;
+
+			default:
+				printf("Invalid Choice!\n");
+				break;
+
+		}
+	}
+	while(choice!=0);
+
+}
