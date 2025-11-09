b247e7c2b63c4997f4616facd837d7d9e5cffec4
RUSHIT SHAH <134259276+RUSHIT305@users.noreply.github.com>
2025-11-09 20:12:03 +0530

    Add custom message to practical4.c
    Added a print statement to display a custom message.

--- practical 4 all files/practical4.c
+++ practical 4 all files/practical4.c
@@ -9,5 +9,7 @@ printf("\nsize of int : %zu",sizeof(i));
 printf("\nsize of float : %zu",sizeof(f));
 printf("\nsize of long int : %zu ",sizeof(long));
 printf("\nsize of char : %zu",sizeof(c));
+printf("rushit shah 25ce110");
 return 0;
 }
+
