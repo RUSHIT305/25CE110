a80bf4779e871b2f50d61be6be78e097ad4e7dad
RUSHIT SHAH <134259276+RUSHIT305@users.noreply.github.com>
2025-11-09 20:15:44 +0530

    Include author name in prac11.c
    Added a print statement for author identification.

--- PRACTICAL 11 ALL FILES/prac11.c
+++ PRACTICAL 11 ALL FILES/prac11.c
@@ -8,7 +8,8 @@ printf("enter your  marks");
 scanf("%d",&marks);
 (marks>=90)? printf("grade a") : (marks>=80 && marks<90) ? printf("grade b") : (marks>=70 && marks<80) ? printf("grade c ") : (marks>=60 && marks<70) ? printf("grade d") : printf("grade fail") ;
 
-
+printf("rushit shah 25ce110");
 
 }
 //code written by rushit shah
+
