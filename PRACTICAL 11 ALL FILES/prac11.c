e0ddceaf71c4e90c24538c6ddcc246c0df4fcd68
RUSHIT SHAH <134259276+RUSHIT305@users.noreply.github.com>
2025-11-12 22:11:00 +0530

    Update prac11.c

--- PRACTICAL 11 ALL FILES/prac11.c
+++ PRACTICAL 11 ALL FILES/prac11.c
@@ -8,8 +8,9 @@ printf("enter your  marks");
 scanf("%d",&marks);
 (marks>=90)? printf("grade a") : (marks>=80 && marks<90) ? printf("grade b") : (marks>=70 && marks<80) ? printf("grade c ") : (marks>=60 && marks<70) ? printf("grade d") : printf("grade fail") ;
 
-printf("rushit shah 25ce110");
+printf("rushit shah 25ce110\n");
 
 }
 //code written by rushit shah
+
 
