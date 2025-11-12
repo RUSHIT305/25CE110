ea25ae1b0e9046795df9f0303f732131c5045fc0
RUSHIT SHAH <134259276+RUSHIT305@users.noreply.github.com>
2025-11-12 20:34:28 +0530

    Update practical4.c

--- practical 4 all files/practical4.c
+++ practical 4 all files/practical4.c
@@ -9,7 +9,8 @@ printf("\nsize of int : %zu",sizeof(i));
 printf("\nsize of float : %zu",sizeof(f));
 printf("\nsize of long int : %zu ",sizeof(long));
 printf("\nsize of char : %zu",sizeof(c));
-printf("rushit shah 25ce110");
+printf("\nrushit shah 25ce110");
 return 0;
 }
+
 
