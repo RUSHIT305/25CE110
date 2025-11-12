1bbafb3115bec7edfbc400fbd38a9d4ba18d17fa
RUSHIT SHAH <134259276+RUSHIT305@users.noreply.github.com>
2025-11-12 20:38:53 +0530

    Update practical6.c

--- practical 6 all files/practical6.c
+++ practical 6 all files/practical6.c
@@ -6,9 +6,10 @@ printf("the celsius is :");
 scanf("%f",&celsius);
 float fahrenheit;
 fahrenheit=(celsius*9/5)+32;
-printf("the value of farenheit is : %f",fahrenheit);
-  printf("rushit shah 25ce110");
+printf("the value of farenheit is : %f\n",fahrenheit);
+  printf("rushit shah 25ce110\n");
 
 return 0;
 }
+
 
