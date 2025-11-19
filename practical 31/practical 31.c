88b1e29277d97e87fbc3f97015833c35374ca533
RUSHIT SHAH <134259276+RUSHIT305@users.noreply.github.com>
2025-11-19 19:26:41 +0530

    Add files via upload

---
+++ practical 31/practical 31.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main()
+{
+    int n=100;
+    char *string=(char*)calloc(n,sizeof(char));
+    printf("Please enter the string you want to store! The current size of the string is %d\n",n);
+    fgets(string,100,stdin);
+    printf("Please enter the size of string you want to resize to\n");
+    scanf("%d",&n);
+    if(n!=0 && n>0)
+    {
+        string = realloc(string,n);
+    }
+    printf("String after memory Reallocation: \n");
+    puts(string);
+    free(string);
+    printf("Name: rushit shah\n Student ID: 25CE110\n");
+}
